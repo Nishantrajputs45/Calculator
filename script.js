13a6fe7226a8dcd89d55d5eb2e04161152fad8d2
Nshantrajputs45 <nishantrajputs45@gmail.com>
2022-10-01 03:54:21 +0530

    Calculator design completed

--- script.js
+++ script.js
@@ -1,14 +1,64 @@
+const textview = document.querySelector(".textview");
 
-function insert(num){
+var currentVal = "";
+var copyCurrentVal = "";
+var preValue = "";
+var number = [];
+var ans;
+
+function insert(digit) {
+  number.push(digit);
+
+  if (number.length != 1) {
+    currentVal = "";
+    textview.value = currentVal;
   }
-  
-  function equals(){
-    
-      
+
+  for (i = 0; i < number.length; i++) {
+    currentVal = currentVal + number[i];
   }
-  
-  function clean(){
+
+  textview.value = currentVal;
+}
+
+function equals() {
+  copyCurrentVal = "";
+  textview.value = "";
+
+  for (i = 0; i < number.length; i++) {
+    copyCurrentVal += number[i];
+    preValue += number[i];
   }
-  
-  function back(){
+
+  ans = eval(copyCurrentVal);
+  copyCurrentVal = ans.toString();
+  preValue = copyCurrentVal;
+  textview.value = ans;
+
+  while (number.length > 0) {
+    number.pop();
   }
+
+  number.push(ans.toString());
+}
+
+function clean() {
+  textview.value = "";
+
+  while (number.length > 0) {
+    number.pop();
+  }
+
+  currentVal = "";
+  copyCurrentVal = "";
+}
+
+function back() {
+  number.pop();
+  currentVal = "";
+  for (i = 0; i < number.length; i++) {
+    currentVal = currentVal + number[i];
+  }
+
+  textview.value = currentVal;
+}
